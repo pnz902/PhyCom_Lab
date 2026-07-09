9bd245b09e2aed39b503d1e358db33b27d8e9925
pnz902 <ananyayaya.ay07@gmail.com>
2026-07-09 23:48:28 +0700

    final ex8

--- Week2/L2_8.c
+++ Week2/L2_8.c
@@ -2,10 +2,12 @@
 
 int main() {
 	char fname1[20], sname1[20], person2[50], person3[50];
-	scanf("%s\n%s\n%s  \n%s", fname1, sname1, person2, person3);
+	scanf("%s\n%s\n", fname1, sname1);
+	scanf("%50[^\n]\n", person2);
+	scanf("%50[^\n]", person3);
 	printf("Person 1: %s %s\n", fname1, sname1);
 	printf("Person 2: %s\n", person2);
 	printf("Person 3: %s", person3);
-
+	
 	return 0;
 }
