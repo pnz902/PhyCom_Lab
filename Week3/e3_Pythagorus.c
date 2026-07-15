dad430feea5d3d712744857c8aa4f5d820502d91
pnz902 <ananyayaya.ay07@gmail.com>
2026-07-15 16:58:16 +0700

    e2-5

---
+++ Week3/e3_Pythagorus.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	double a, b;
+    scanf("%lf\n%lf", &a, &b);
+    printf("%.2lf", sqrt(pow(a,2)+pow(b,2)));
+
+	return 0;
+}
---
+++ Week3/e4_Arithmetic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	double n1, n2;
+    scanf("%lf,%lf", &n1, &n2);
+    printf("The sum of the given numbers : %lf\n", n1+n2);
+    printf("The difference of the given numbers : %lf\n", n1-n2);
+    printf("The product of the given numbers : %lf\n", n1*n2);
+    printf("The quotient of the given numbers : %lf", n1/n2);
+    
+	return 0;
+}
